c72bcbde78a342e4ffad5bb1c3405af9e54641e2
Tanya Ignatenko <alisainy@gmail.com>
2018-06-13 23:07:28 +0700

    Make ball move

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canv
 let context: CanvasRenderingContext2D = canvas.getContext('2d');
 let x: number = 10;
 let y: number = 10;
+let dx: number = 1;
+let dy: number = 1;
 let r: number = 10;
 let ballColor: string = "white";
 
@@ -13,8 +15,11 @@ function drawBall(x: number, y: number, r: number, ballColor: string): void {
 }
 
 function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall(x, y, r, ballColor);
-
+    x += dx;
+    y += dy;
+    window.requestAnimationFrame(draw);
 }
 
 window.requestAnimationFrame(draw);
